e1463b31263e3317b9e7d625e4346b294ffd0471
Jenny K <jkam17@gmail.com>
2018-07-20 16:21:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,23 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(s){
+<<<<<<< HEAD
   if (s===(s.toUpperCase())){
     return "YES INDEED!"
+=======
+  if (s===(s.toLowerCase)){
+    return "I can't hear you!"
+>>>>>>> ed42a8cb4f9f286694b07d8df85b0155337602c5
   }
   else if(s==="I love you, Grandma."){
     return "I love you, too."
   }
   else{
+<<<<<<< HEAD
     return "I can't hear you!"
+=======
+    return "YES INDEED!"
+>>>>>>> ed42a8cb4f9f286694b07d8df85b0155337602c5
   }
 }
 
